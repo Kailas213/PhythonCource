bf4b23e7677aa8d7f02d6d419198c54ed3510e4c
Kailas Kupate <kupatekailas213@gmail.com>
2025-11-24 12:40:24 +0530

    changes

--- FirstProject/Task3.py
+++ FirstProject/Task3.py
@@ -1,23 +1,5 @@
-<<<<<<< HEAD
-def factorial(factNum):
-    fact=1
-    for i in range(1,factNum+1):
-        fact =fact*i
-    return fact
-
-factNum = int(input("Enter a number:"))
-result=factorial(factNum)
-print(f"Factorial of {factNum} is {result}")
-
-
-
-
-
-
-=======
 num=int(input("Enter a number: "))
 if num%2==0:
     print(f"{num} is an even number.")
 else:
     print(f"{num} is an odd number.")
->>>>>>> b2f3e97f1a15382fd7c177fb6d64a252862efc5f
