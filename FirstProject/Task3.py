2436df826215091048eabcf6c4a4cd864eef4e4d
Kailas Kupate <kupatekailas213@gmail.com>
2025-11-24 12:36:51 +0530

    Module 4: Functions & Modules in Python  Task 1 Assignment

---
+++ FirstProject/Task3.py
@@ -0,0 +1,15 @@
+def factorial(factNum):
+    fact=1
+    for i in range(1,factNum+1):
+        fact =fact*i
+    return fact
+
+factNum = int(input("Enter a number:"))
+result=factorial(factNum)
+print(f"Factorial of {factNum} is {result}")
+
+
+
+
+
+
